1e25cda87fd0feb46a6600aeefd21711925923c6
yui <ycui@microstrategy.com>
2017-12-11 12:59:21 +0800

    add resource files

--- myspark/src/main/java/com/skygon/spark/DataStructure.java
+++ myspark/src/main/java/com/skygon/spark/DataStructure.java
@@ -1,9 +1,11 @@
 package com.skygon.spark;
 
-import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
 
 	
 public class DataStructure {
-	SparkSession s;
+	
+	public static void main(String[] args){
+		
+	}
 }
