0aa3c1236529cc53e1213255c5baa32f65359e76
skygon <skygoncui@163.com>
2017-12-11 09:04:02 +0800

    init maven project

---
+++ myspark/src/main/java/com/skygon/spark/App.java
@@ -0,0 +1,13 @@
+package com.skygon.spark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
