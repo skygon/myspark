29d86093ee724d93e57be1e3a83a2ee7cf644de5
yui <ycui@microstrategy.com>
2017-12-29 16:28:06 +0800

    update

--- myspark/src/main/java/com/skygon/spark/DataStructure.java
+++ myspark/src/main/java/com/skygon/spark/DataStructure.java
@@ -1,6 +1,11 @@
 package com.skygon.spark;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.spark.SparkConf;
+import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
@@ -36,13 +41,27 @@ public class DataStructure {
 		//RowFactory.create(values)
 	}
 
-	public static void main( String[] args ){
+	public static void main( String[] args ) throws InterruptedException{
 		DataStructure ds = new DataStructure();
 		
-		Dataset<Row> df = ds.readFromFile("src/resource/people.json", FileType.JSON);
-		//df.show();
-		df.explain();
+		Dataset<Row> df = ds.readFromFile("src/resource/JsonlargeFile.json", FileType.JSON);
+		Dataset<Row> df2 = ds.readFromFile("src/resource/people.json", FileType.JSON);
+		df.show();
+		Dataset<Row> df3 = df.select("email");
+		df3.count();
+		//df.explain();
 		System.out.println(ds.mSparkSession.conf().getAll());
-		
+
+		int i = 0;
+		String[] col = df.columns();
+		List<RDD<Row>> dfs = new LinkedList<RDD<Row>>();
+		while (true){
+			TimeUnit.SECONDS.sleep(5);
+			dfs.add(df.select(col[i]).rdd());
+			i ++;
+			if (i >= col.length){
+				i = 0;
+			}
+		}
 	}
 }
