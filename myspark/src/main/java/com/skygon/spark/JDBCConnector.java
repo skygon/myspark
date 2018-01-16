08a5e699ba8ff559dac1367d85c3dc887e8c2f20
yui <ycui@microstrategy.com>
2018-01-16 13:46:48 +0800

    update

--- myspark/src/main/java/com/skygon/spark/JDBCConnector.java
+++ myspark/src/main/java/com/skygon/spark/JDBCConnector.java
@@ -7,6 +7,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.StructType;
 
 public class JDBCConnector {
 	private SparkSession mSparkSession = null;
@@ -16,11 +17,16 @@ public class JDBCConnector {
 		SparkConf sparkConf = new SparkConf()
 				.setAppName("JDBCConnector")
 				.setMaster("spark://10.197.38.95:7077");
+				//.setMaster("local[*]");
 		
 		this.mSparkSession = SparkSession.builder()
 				.config(sparkConf)
 				.getOrCreate();
-				
+		
+		// schema is retrieved at driver program. That's why app still works without addjar.
+		// But actions on RDD launch at work node and has no context of mysql jdbc driver,
+		// that's why we need to add jar files to spark context.
+		this.mSparkSession.sparkContext().addJar("file:///E:/Java/project/myspark/myspark/lib/mysql-connector-java-5.1.44-bin.jar");
 	}
 	
 	void setOptions(){
@@ -34,6 +40,9 @@ public class JDBCConnector {
 	void loadData(){
 		setOptions();
 		Dataset<Row> df = mSparkSession.read().format("jdbc").options(options).load();
+		StructType schema = df.schema();
+		df.show();
+		System.out.println("schema is " + schema.toString());
 	}
 	public static void main(String[] args){
 		System.out.println("=== start app ===");
